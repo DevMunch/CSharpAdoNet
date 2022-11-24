ee8a49fff6c9e7e20adf60f7afa305ba74a81f6d
Munch <dev.coder.munch@gmail.com>
2022-11-24 14:30:14 +0900

    Start ADO.NET
    1. Connection
    2. App.config

---
+++ AdoNetWindow/AdoNetWindow/Form1.cs
@@ -0,0 +1,27 @@
+using AdoNet;
+using AppConfiguration;
+
+namespace AdoNetWindow
+{
+    public partial class Form1 : Form
+    {
+        MsSql msql;
+        string ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConfigurationMgr instance = ConfigurationMgr.Instance();
+            MessageBox.Show(instance.Connection.State.ToString());
+            instance.Connection.Close();
+        }
+    }
+}
---
+++ AdoNetWindow/AppConfiguration/ConfigurationMgr.cs
@@ -0,0 +1,43 @@
+﻿using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AppConfiguration
+{
+    public class ConfigurationMgr
+    {
+        private static ConfigurationMgr instance;
+        public string ConnectionString { get; set; }
+        IDbConnection connection;
+        public IDbConnection Connection
+        {
+            get 
+            {
+                if(connection == null)
+                    connection = new SqlConnection(ConnectionString);
+                if(connection.State != ConnectionState.Open) // 인스턴스는 있지만 닫혀있다면.
+                    connection.Open();
+                return connection;
+            }
+            private set { }
+        }
+        private ConfigurationMgr()
+        {
+            // DB접속을 위한 파일을 읽어온다.
+            LoadConfiguration();
+        }
+
+        private void LoadConfiguration()
+        {
+            // App.config를 읽어온다.
+            Configuration currentConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            ConnectionString = currentConfig.ConnectionStrings.ConnectionStrings["WinConnection"].ConnectionString;
+        }
+        public static ConfigurationMgr Instance()
+        {
+            if(instance == null)
+                instance = new ConfigurationMgr();
+            return instance;
+        }
+    }
+}
